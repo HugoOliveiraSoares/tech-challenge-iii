ad17186bf0b105f39c6ed7a7201e054463406903
msousa-s <matheussousa_santos@yahoo.com>
2026-04-19 15:46:04 -0300

    Feature:(Estruturação de pastas)

---
+++ pagamento-service/src/main/java/br/com/fiap/tech_challenge_iii/TechChallengeIiiAplication.java
@@ -0,0 +1,11 @@
+package br.com.fiap.tech_challenge_iii;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechChallengeIiiAplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechChallengeIiiAplication.class, args);}
+}
