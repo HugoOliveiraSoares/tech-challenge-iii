a2ea0ec80bc56ded9559c16ce23955371573c3cd
msousa-s <matheussousa_santos@yahoo.com>
2026-04-19 21:33:31 -0300

    Feature:(User e Pedido)

---
+++ pagamento-service/src/main/java/br/com/fiap/tech_challenge_iii/core/domain/Pedido.java
@@ -0,0 +1,23 @@
+package br.com.fiap.tech_challenge_iii.core.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Pedido {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id1;
+    private Long id;
+    private String nome;
+    private Long quantidade;
+    private Double valor;
+}
---
+++ pagamento-service/src/main/java/br/com/fiap/tech_challenge_iii/core/domain/valueObjects/User.java
@@ -0,0 +1,7 @@
+package br.com.fiap.tech_challenge_iii.core.domain.valueObjects;
+
+public class User {
+    private Long id;
+    private String nome;
+    private String email;
+}
