47d410fbf09cada0d3c9607a724a98b2e4e7c0f9
Lucas Oliveira <luc.osilva@gmail.com>
2026-04-21 00:02:46 -0300

    add password encryption with bcrypt

--- auth-service/src/main/java/br/com/fiap/auth/core/usecase/impl/CreateUserUseCaseImpl.java
+++ auth-service/src/main/java/br/com/fiap/auth/core/usecase/impl/CreateUserUseCaseImpl.java
@@ -7,23 +7,26 @@ import br.com.fiap.auth.core.exception.EmailAlreadyInUseException;
 import br.com.fiap.auth.core.gateway.UserGateway;
 import br.com.fiap.auth.core.usecase.CreateUserUseCase;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @RequiredArgsConstructor
 public class CreateUserUseCaseImpl implements CreateUserUseCase {
 
     private final UserGateway userGateway;
+    private final PasswordEncoder passwordEncoder;
 
     @Override
     public CreateUserOutput createUser(CreateUserInput input) {
 
         var email = input.email().toLowerCase().trim();
         if(userGateway.existsByEmail(email)){
-            throw new EmailAlreadyInUseException("Email %s is already in use".formatted(email));
+            throw new EmailAlreadyInUseException("Email '%s' is already in use".formatted(email));
         }
 
+        var password = passwordEncoder.encode(input.password());
         var newUser = new User(input.name(),
                 email,
-                input.password(),
+                password,
                 input.role());
 
         var saved = userGateway.save(newUser);
