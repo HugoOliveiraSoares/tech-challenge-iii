253e6382aff7984524a495ffd1c830d7d7c9b387
Lucas Oliveira <luc.osilva@gmail.com>
2026-04-30 16:49:38 -0300

    add GlobalExceptionHandler with standardized error response

--- auth-service/src/main/java/br/com/fiap/auth/core/exception/EmailAlreadyInUseException.java
+++ auth-service/src/main/java/br/com/fiap/auth/core/exception/EmailAlreadyInUseException.java
@@ -1,7 +1,7 @@
 package br.com.fiap.auth.core.exception;
 
 public class EmailAlreadyInUseException extends SystemBaseException {
-    private static final String CODE = "user.actionNotAllowed";
+    private static final String CODE = "user.email_already_exists";
     private static final Integer HTTP_STATUS = 409;
 
     public EmailAlreadyInUseException(String message) {
--- auth-service/src/main/java/br/com/fiap/auth/core/exception/InvalidCredentialsException.java
+++ auth-service/src/main/java/br/com/fiap/auth/core/exception/InvalidCredentialsException.java
@@ -1,7 +1,7 @@
 package br.com.fiap.auth.core.exception;
 
 public class InvalidCredentialsException extends SystemBaseException {
-    public static final String CODE = "auth.invalidCredentials";
+    public static final String CODE = "auth.invalid_credentials";
     public static final String MESSAGE = "Invalid username or password";
     public static final Integer HTTP_STATUS = 401;
 
