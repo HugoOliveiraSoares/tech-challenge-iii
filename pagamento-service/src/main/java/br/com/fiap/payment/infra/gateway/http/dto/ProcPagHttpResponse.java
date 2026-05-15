0bf2dad2d5b22e79829424f1890a9d812edec1bb
HugoOliveiraSoares <hugoliveira.soares@gmail.com>
2026-05-14 22:37:15 -0300

    fix procpag response class

--- pagamento-service/src/main/java/br/com/fiap/payment/infra/gateway/http/dto/ProcPagHttpResponse.java
+++ pagamento-service/src/main/java/br/com/fiap/payment/infra/gateway/http/dto/ProcPagHttpResponse.java
@@ -3,6 +3,5 @@ package br.com.fiap.payment.infra.gateway.http.dto;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public record ProcPagHttpResponse(
-    @JsonProperty("pagamento_id") String pagamentoId,
-    @JsonProperty("status") String status
-) {}
+        @JsonProperty("status") String status) {
+}
