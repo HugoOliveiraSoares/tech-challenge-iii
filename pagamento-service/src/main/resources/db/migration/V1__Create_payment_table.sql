a44e260d396ded49640480fe943e7f326bc079d4
HugoOliveiraSoares <hugoliveira.soares@gmail.com>
2026-05-21 20:42:42 -0300

    unique order_id

--- pagamento-service/src/main/resources/db/migration/V1__Create_payment_table.sql
+++ pagamento-service/src/main/resources/db/migration/V1__Create_payment_table.sql
@@ -1,6 +1,6 @@
 CREATE TABLE payment (
     payment_id BIGSERIAL PRIMARY KEY,
-    order_id VARCHAR(255) NOT NULL,
+    order_id VARCHAR(255) NOT NULL UNIQUE,
     client_id VARCHAR(255) NOT NULL,
     total_amount DECIMAL(19,2) NOT NULL,
     payment_status VARCHAR(20) NOT NULL,
