35b8c9deac909ce2c38051ee6c19e6b40e365c10
HugoOliveiraSoares <hugoliveira.soares@gmail.com>
2026-05-23 18:48:40 -0300

    remove unused class

--- pagamento-service/src/main/java/br/com/fiap/payment/core/usecase/PublishPaymentUseCase.java
+++
@@ -1,7 +0,0 @@
-package br.com.fiap.payment.core.usecase;
-
-import br.com.fiap.payment.core.domain.PaymentEvent;
-
-public interface PublishPaymentUseCase {
-    void execute(PaymentEvent event);
-}
